765ec02829aaef4a7c2927639cb808970f2e1e3e
Raphael Vigneault Ð <68454661+Izocel@users.noreply.github.com>
2023-10-25 15:12:27 -0400

    Update CardHelper.ts

--- src/tools/class/CardHelper.ts
+++ src/tools/class/CardHelper.ts
@@ -7,19 +7,19 @@ export class CardHelper {
 
     public attachment: BufferResolvable | internal.Stream
 
-    async render(member: GuildMember, string: title = "Welcome") {
-        const welcomeCard = new Canvas.Welcome();
-        welcomeCard.setUsername(member.displayName)
+    async render(member: GuildMember, title: string) {
+        const card = new Canvas.Welcome();
+        card.setUsername(member.displayName)
             .setAvatar(member.user.displayAvatarURL({ forceStatic: true, extension: ALLOWED_EXTENSIONS[1] }))
             .setGuildName(member.guild.name)
             .setMemberCount(member.guild.memberCount)
             .setDiscriminator(member.guild.memberCount);
 
-        welcomeCard.textTitle = title;
-        welcomeCard.textMessage = `Welcome to ${member.guild.name}`;
-        theme.setRndWelcomeStyle(welcomeCard);
+        card.textTitle = title ?? "Welcome";
+        card.textMessage = `Welcome to ${member.guild.name}`;
+        theme.setRndWelcomeStyle(card);
 
-        const image = await welcomeCard.toAttachment();
+        const image = await card.toAttachment();
         this.attachment = new AttachmentBuilder(image.toBuffer()).attachment;
         return this;
     }
