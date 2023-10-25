1b5489bb213426ec87976849e9cae0778808096e
Raphael Vigneault Ð. <68454661+Izocel@users.noreply.github.com>
2023-10-25 22:58:14 +0000

    ...

--- src/class/DiscordManager.ts
+++ src/class/DiscordManager.ts
@@ -1,10 +1,14 @@
-import { GuildBasedChannel, GuildTextBasedChannel, MessageCreateOptions, MessagePayload, PartialTextBasedChannel, TextBasedChannel } from "discord.js"
+import { GuildBasedChannel, GuildTextBasedChannel, MessageCreateOptions, MessagePayload } from "discord.js"
 
 export class DiscordManager {
 
     public static async guildSend(channel: GuildBasedChannel, content: string | MessagePayload | MessageCreateOptions) {
-        channel = channel as GuildTextBasedChannel;
-        channel.sendTyping();
-        await channel.send(content)
+        try {
+            channel = channel as GuildTextBasedChannel;
+            channel.sendTyping();
+            await channel.send(content);
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
