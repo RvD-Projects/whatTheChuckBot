e2be7168e81bc1cdac262d9e7c34e60e7b38628c
WeConnected <40178706+CtrlAiDel@users.noreply.github.com>
2024-03-30 16:35:23 -0400

    Update messageCreateOllama.ts

--- src/events/messageCreateOllama.ts
+++ src/events/messageCreateOllama.ts
@@ -86,7 +86,9 @@ async function chat(
   const data = await response.json();
   const message = data?.message;
   const text = message?.content;
-
+  if (!text) {
+    return "(void)";
+  }
   messages.push(message);
   messagesState.set(author.id, messages);
   return text;
