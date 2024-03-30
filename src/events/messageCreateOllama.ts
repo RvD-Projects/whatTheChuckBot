2fb41b7d24f2073b1319b48236c56e3e3bd0b9bc
WeConnected <yvanlincroyable@gmail.com>
2024-03-30 16:37:20 -0400

    correction un if était deleter

--- src/events/messageCreateOllama.ts
+++ src/events/messageCreateOllama.ts
@@ -83,6 +83,9 @@ async function chat(
     `${configs.url}/chat`,
     JSON.stringify(options)
   );
+  if (!text) {
+    return "(void)";
+  }
   const data = await response.json();
   const message = data?.message;
   const text = message?.content;
