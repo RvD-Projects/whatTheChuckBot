e6a4b5b6bb672d29cf394a5e6ce98ba973c31f27
Raphael Vigneault Ð <triangle1232009@hotmail.com>
2024-04-20 17:30:19 -0400

    ...

--- environments.d.ts
+++ environments.d.ts
@@ -2,14 +2,14 @@
 declare global {
     namespace NodeJS {
         interface ProcessEnv {
+            environment: "dev" | "prod";
             botToken: string;
             guildIds: string;
             inviteUrl: string;
             RCON_PASS_192_168_1_128_27015:string;
             RCON_PASS_192_168_1_128_27016:string;
             DB_CONN_STRING:string,
-            DB_NAME:string
-            environment: "dev" | "prod";
+            DB_NAME:string,
         }
     }
 }
