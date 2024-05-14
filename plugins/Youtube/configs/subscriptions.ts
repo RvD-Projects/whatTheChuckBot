2f86cc926f6c5dabb8f3acb2b1a33d336ba3b634
Raphael Vigneault Ð <triangle1232009@hotmail.com>
2024-05-13 22:52:59 -0400

    ...

---
+++ plugins/Youtube/configs/subscriptions.ts
@@ -0,0 +1,255 @@
+const subscriptions = {
+  servers: [
+    {
+      id: "276931890735218689",
+      name: "RVÐ-Projects",
+      devOnly: false,
+      channels: [
+        {
+          id: "1139624864475525211",
+          mentionRoleId: "1021054460438523976",
+          subs: [
+            {
+              name: "@Underscore_",
+              url: "https://www.youtube.com/@Underscore_",
+            },
+          ]
+        },
+      ],
+    },
+    {
+      id: "984305093010673684",
+      name: "FC/WE - Live Looping",
+      channels: [
+        {
+          id: "1138019825311023114",
+          mentionRoleId: "1139637488911130726",
+          subs: [
+            {
+              name: "@CyranMakesMusic",
+              url: "https://www.youtube.com/@CyranMakesMusic/videos",
+            },
+            {
+              name: "@FrankTedesco-Highlights",
+              url: "https://www.youtube.com/@FrankTedescoHighlights/videos",
+            },
+            {
+              name: "@FrankTedesco",
+              url: "https://www.youtube.com/@FrankTedescoOfficial/videos",
+            },
+            {
+              name: "@FrankTedesco-VODS",
+              url: "https://www.youtube.com/@FrankTedescoVODs/videos",
+            },
+            {
+              name: "@TheKiffness",
+              url: "https://www.youtube.com/@TheKiffness/videos",
+            },
+            {
+              name: "@ReinhardtBuhr",
+              url: "https://www.youtube.com/@ReinhardtBuhr/videos",
+            },
+            {
+              name: "@NeonVines",
+              url: "https://www.youtube.com/@NeonVines/videos",
+            },
+            {
+              name: "@MarcusVeltri",
+              url: "https://www.youtube.com/@MarcusVeltri/videos",
+            },
+            {
+              name: "@ZoeWrenTwitchVODs",
+              url: "https://www.youtube.com/@ZoeWrenTwitchVODs/videos",
+            },
+            {
+              name: "@MrWobbles",
+              url: "https://www.youtube.com/@MrWobbles/videos",
+            },
+            {
+              name: "@Justine-Griffin",
+              url: "https://www.youtube.com/@Justine-Griffin/videos",
+            },
+            {
+              name: "@MilesMusicKid",
+              url: "https://www.youtube.com/@milesmusickid/videos",
+            },
+            {
+              name: "@DovydasMusic",
+              url: "https://www.youtube.com/@DovydasMusic/videos",
+            },
+          ]
+        },
+        {
+          id: "1139621115396890726",
+          mentionRoleId: "1139637848681742386",
+          subs: [
+            {
+              name: "@Sarobeatbox",
+              url: "https://www.youtube.com/@Sarobeatbox/videos",
+            },
+            {
+              name: "@MB14BEATBOX",
+              url: "https://www.youtube.com/@MB14BEATBOX/videos",
+            },
+            {
+              name: "@swissbeatbox",
+              url: "https://www.youtube.com/@swissbeatbox/videos",
+            },
+            {
+              name: "@matej_music",
+              url: "https://www.youtube.com/@matej_music/videos",
+            },
+            {
+              name: "@BEATBOXTELEVISION",
+              url: "https://www.youtube.com/@BEATBOXTELEVISION/videos",
+            },
+          ]
+        },
+        {
+          id: "1139621690821840986",
+          mentionRoleId: "1139638023680708688",
+          subs: [
+            {
+              name: "@HarryMack",
+              url: "https://www.youtube.com/@HarryMack/videos",
+            },
+            // {
+            //   name: "@HarryMack-Clips",
+            //   url: "https://www.youtube.com/@HarryMackClips/videos",
+            // },
+            {
+              name: "@MarcRebillet-WeOutside",
+              url: "https://www.youtube.com/playlist?list=PL_AprguVzvxHdlFAvJp6LXW2WLvNPe6m7",
+            },
+            {
+              name: "@MarcRebillet-Full-Music-Streams",
+              url: "https://www.youtube.com/playlist?list=PL_AprguVzvxF1d6OSMiLC7MDo8pIxsQxE",
+            },
+            {
+              name: "@ChrisPJTurner",
+              url: "https://www.youtube.com/@ChrisPJTurner/videos",
+            }
+          ]
+        }
+      ]
+    },
+    {
+      id: "1197480594284756993",
+      name: "Last RC-505 showdown!",
+      channels: [
+        {
+          id: "1198368139314999386",
+          mentionRoleId: "1198370173871198318",
+          subs: [
+            {
+              name: "@CyranMakesMusic",
+              url: "https://www.youtube.com/@CyranMakesMusic/videos",
+            },
+            {
+              name: "@FrankTedesco-Highlights",
+              url: "https://www.youtube.com/@FrankTedescoHighlights/videos",
+            },
+            {
+              name: "@FrankTedesco",
+              url: "https://www.youtube.com/@FrankTedescoOfficial/videos",
+            },
+            {
+              name: "@FrankTedesco-VODS",
+              url: "https://www.youtube.com/@FrankTedescoVODs/videos",
+            },
+            {
+              name: "@TheKiffness",
+              url: "https://www.youtube.com/@TheKiffness/videos",
+            },
+            {
+              name: "@ReinhardtBuhr",
+              url: "https://www.youtube.com/@ReinhardtBuhr/videos",
+            },
+            {
+              name: "@NeonVines",
+              url: "https://www.youtube.com/@NeonVines/videos",
+            },
+            {
+              name: "@MarcusVeltri",
+              url: "https://www.youtube.com/@MarcusVeltri/videos",
+            },
+            {
+              name: "@ZoeWrenTwitchVODs",
+              url: "https://www.youtube.com/@ZoeWrenTwitchVODs/videos",
+            },
+            {
+              name: "@MrWobbles",
+              url: "https://www.youtube.com/@MrWobbles/videos",
+            },
+            {
+              name: "@Justine-Griffin",
+              url: "https://www.youtube.com/@Justine-Griffin/videos",
+            },
+            {
+              name: "@MilesMusicKid",
+              url: "https://www.youtube.com/@milesmusickid/videos",
+            },
+            {
+              name: "@DovydasMusic",
+              url: "https://www.youtube.com/@DovydasMusic/videos",
+            },
+            {
+              name: "@matej_music",
+              url: "https://www.youtube.com/@matej_music/videos",
+            },
+          ]
+        },
+        {
+          id: "1198368484476858439",
+          mentionRoleId: "1198370227038191667",
+          subs: [
+            {
+              name: "@Sarobeatbox",
+              url: "https://www.youtube.com/@Sarobeatbox/videos",
+            },
+            {
+              name: "@MB14BEATBOX",
+              url: "https://www.youtube.com/@MB14BEATBOX/videos",
+            },
+            {
+              name: "@swissbeatbox",
+              url: "https://www.youtube.com/@swissbeatbox/videos",
+            },
+            {
+              name: "@BEATBOXTELEVISION",
+              url: "https://www.youtube.com/@BEATBOXTELEVISION/videos",
+            },
+          ]
+        },
+        {
+          id: "1198368553372483614",
+          mentionRoleId: "1198372438464016526",
+          subs: [
+            {
+              name: "@HarryMack",
+              url: "https://www.youtube.com/@HarryMack/videos",
+            },
+            // {
+            //   name: "@HarryMack-Clips",
+            //   url: "https://www.youtube.com/@HarryMackClips/videos",
+            // },
+            {
+              name: "@MarcRebillet-WeOutside",
+              url: "https://www.youtube.com/playlist?list=PL_AprguVzvxHdlFAvJp6LXW2WLvNPe6m7",
+            },
+            {
+              name: "@MarcRebillet-Full-Music-Streams",
+              url: "https://www.youtube.com/playlist?list=PL_AprguVzvxF1d6OSMiLC7MDo8pIxsQxE",
+            },
+            {
+              name: "@ChrisPJTurner",
+              url: "https://www.youtube.com/@ChrisPJTurner/videos",
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
+
+export default subscriptions;
